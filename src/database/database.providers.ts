450aac791ca9908c35650682303ba671a2cdf1c5
soyoung kim <soyou6358@gmail.com>
2021-03-04 22:34:14 +0900

    sqlite database

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,12 +5,11 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () =>
       await createConnection({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'test',
+        type: 'sqlite',
+        database: './database.sql',
+        // username: 'root',
+        // password: 'root',
+        // database: 'test',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
